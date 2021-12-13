4c31fd19aa9fe26147b224bc600abe3239d98333
sofianbello <safian.bello@haw-hamburg.de>
2021-12-13 15:16:36 +0100

    Initialize New Project

---
+++ 13_12/components/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav>
+        <div>Home</div>
+        <div>Über mich</div>
+        <div>Kontakt</div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 20px;
+    padding: 30px 0;
+}
+
+nav div {
+    font-size: 1.5rem;
+}
+
+</style>
